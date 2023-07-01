72cbf9c93daab7bf2770b08c9e7fb8a9a801a398
Felix Wegener <Felix.Wegener05@t-online.de>
2023-07-01 11:23:17 +0200

    del unused function

--- sensor.py
+++ sensor.py
@@ -3,15 +3,6 @@ from time import sleep
 
 sensor = Adafruit_DHT.DHT11
 pin = 14
-
-def getInfo(): 
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-    sleep(1.5)
-
-    if humidity is not None and temperature is not None:
-        return temperature
-    else:
-        return 'Sensor Error'
     
 def getTemperature():
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
